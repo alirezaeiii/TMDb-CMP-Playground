a1d401453aef9994ea3450d8673723e1bd10b6d3
Ali <ali.rezaei09@gmail.com>
2024-08-06 01:32:26 +0330

    Implement Detail TopBar with Material3

--- composeApp/src/commonMain/kotlin/component/DisneyTopBar.kt
+++ composeApp/src/commonMain/kotlin/component/DisneyTopBar.kt
@@ -1,19 +1,22 @@
 package component
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.sp
 import disney_compose_multiplatform.composeapp.generated.resources.Res
 import disney_compose_multiplatform.composeapp.generated.resources.app_name
 import org.jetbrains.compose.resources.stringResource
@@ -22,29 +25,56 @@ import org.jetbrains.compose.resources.stringResource
 @Composable
 fun DisneyTopBar(scrollContent: @Composable (PaddingValues) -> Unit) {
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
-
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
-                    containerColor = MaterialTheme.colors.background,
-                    scrolledContainerColor = MaterialTheme.colors.background
-                ),
-                title = {
-                    Text(
-                        stringResource(Res.string.app_name),
-                        maxLines = 1,
-                        overflow = TextOverflow.Ellipsis,
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colors.onSurface
-                    )
-                },
-                scrollBehavior = scrollBehavior
-            )
-        },
+            CenterAlignedTopBar(stringResource(Res.string.app_name), scrollBehavior)
+        }
     ) {
         scrollContent(it)
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DisneyDetailTopBar(title: String, onNavigationIconClick: () -> Unit) {
+    Surface {
+        CenterAlignedTopBar(title) {
+            IconButton(
+                onNavigationIconClick
+            ) {
+                Icon(
+                    Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "back",
+                    tint = MaterialTheme.colors.onSurface
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun CenterAlignedTopBar(
+    title: String,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigationIcon: @Composable () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colors.background,
+            scrolledContainerColor = MaterialTheme.colors.background
+        ),
+        title = {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.h6,
+                color = MaterialTheme.colors.onSurface
+            )
+        },
+        navigationIcon = {
+            navigationIcon.invoke()
+        },
+        scrollBehavior = scrollBehavior
+    )
 }
--- composeApp/src/commonMain/kotlin/ui/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/DetailScreen.kt
@@ -19,16 +19,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.runtime.Composable
@@ -36,7 +33,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
@@ -46,6 +42,7 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
 import component.DetailsScroller
+import component.DisneyDetailTopBar
 import component.ToolbarState
 import component.isShown
 import domain.model.Poster
@@ -156,46 +153,14 @@ private fun DetailsToolbar(
     contentAlpha: () -> Float
 ) {
     if (toolbarState.isShown) {
-        DetailsToolbar(
-            name = name,
-            onBackClick = pressOnBack,
-        )
+        DisneyDetailTopBar(name) {
+            pressOnBack.invoke()
+        }
     } else {
         HeaderActions(
             onBackClick = pressOnBack,
             modifier = Modifier.alpha(contentAlpha())
         )
-    }
-}
-
-@Composable
-private fun DetailsToolbar(
-    name: String,
-    onBackClick: () -> Unit
-) {
-    Surface {
-        TopAppBar(
-            backgroundColor = MaterialTheme.colors.surface
-        ) {
-            IconButton(
-                onBackClick,
-                Modifier.align(Alignment.CenterVertically)
-            ) {
-                Icon(
-                    Icons.AutoMirrored.Filled.ArrowBack,
-                    contentDescription = "back",
-                    tint = MaterialTheme.colors.onSurface
-                )
-            }
-            Text(
-                text = name,
-                style = MaterialTheme.typography.h6,
-                modifier = Modifier
-                    .weight(1f)
-                    .fillMaxSize()
-                    .wrapContentSize(Alignment.CenterStart)
-            )
-        }
     }
 }
 
